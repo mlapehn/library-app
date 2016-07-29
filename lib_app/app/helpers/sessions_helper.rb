675bd2865cbc3899231420a2a25dea1eb33e0c96
Matthew John Lapehn <mlapehn@gmail.com>
2016-07-29 16:18:01 -0600

    Added Create in sessions controller

--- lib_app/app/helpers/sessions_helper.rb
+++ lib_app/app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
 	end
 
 	def current_user
-		@current_user || = User.find(sessions[:user_id])
+		@current_user ||= User.find(sessions[:user_id])
 	end
 
 	def logged_id?
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "users#index"
+
   get "/sign_in", to: "sessions#new"
+
+  post "/sessions", to: "sessions#create"
+
 end
