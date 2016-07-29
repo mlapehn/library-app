24f2095041c520c5ae6c26bc2f461cde60201544
Matthew John Lapehn <mlapehn@gmail.com>
2016-07-29 15:56:54 -0600

    Added sessions helper logic

--- lib_app/app/helpers/sessions_helper.rb
+++ lib_app/app/helpers/sessions_helper.rb
@@ -1,2 +1,21 @@
 module SessionsHelper
+	def login(user)
+		session[:user_id] = user.user_id
+		@current_user = user
+	end
+
+	def current_user
+		@current_user || = User.find(sessions[:user_id])
+	end
+
+	def logged_id?
+		if current_user = nil
+			redirect_to "/sign_in"
+		end
+	end
+
+	def logout
+		@current_user = session[:user_id] = nil
+	end
+	
 end
